a8b2889a242c7c2a5af40587835f53b6ecb1d673
Dulguun <70651482+D8L788H@users.noreply.github.com>
2023-06-08 19:01:53 +0900

    Update main.py
    Fixed it by adding every single letter that needs to be added into a list (by multiplying each letter by the column height) and adding the letters to the tube from that list

--- idlsidgo/main.py
+++ idlsidgo/main.py
@@ -1,58 +1,45 @@
 from bruh import Tube
-from random import randrange
-from random import choice
+from random import randrange, choice
+import logging
 
 # get input and create tubes
-values = ['a', 'b', 'c', 'd']
+letters = ['a', 'b', 'c', 'd']
+let_count = [0, 0, 0, 0]
 while True:
     diff = input('1: Easy\n2: Medium\n3: Hard\n')
     if diff == '1':
         num = 5
-        total = 20
         break
     elif diff == '2':
         num = 6
-        total = 30
-        values.append('e')
+        letters.append('e')
+        let_count.append(0)
         break
     elif diff == '3':
         num = 7
-        total = 42
-        values.append('e')
-        values.append('f')
+        letters.append('e')
+        letters.append('f')
+        let_count.append(0)
+        let_count.append(0)
         break
+total = num * num - num
 tubes = Tube(num, num)
 
-# fill tubes
-
-length = [0]*num
 letter = []
+for x in letters:
+    for y in range(num):
+        letter.append(x)
+# fill tubes
 for times in range(total):
-    # get a random letter from the values list, if the number of same
-    # letters exceeds the column number, that letters gets removed
-    # from the values list.
-    x = choice(values)
-    letter.append(x)
-    if letter.count(x) == num:
-        values.remove(x)
+    value = choice(letter)
+    letter.remove(value)
+    while True:
+        try:
+            key = randrange(num)
+            tubes.add(value, key)
+            break
+        except Exception:
+            pass
+    # output
+    print(tubes, '\n')
 
-    # get a random value for the position and add 1 to that position, if the
-    # value of that position exceeds the num (length of the column), it gets
-    # a new random value
-    y = randrange(num)
-    length[y] += 1
-    if length[y] >= num:
-        while True:
-            new_y = randrange(num)
-            if new_y != y:
-                y = new_y
-                break
-        length[y] += 1
-        
-    # add the random values to the tubes using add method
-    tubes.add(x, y, 1)
-    # haaya ene deed taliin method add hiih ystoigoosoo 1 ymuu 2oor baga useg add hiigeed baih ym
-    # yagaad gedgiig n uneheer oilgodgue
-
-print()
-print(tubes)
